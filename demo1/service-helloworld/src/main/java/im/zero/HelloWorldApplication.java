17c0c26bb2580ae42da6645e86fba157d4275b3a
Zero Wang <zero@cnshdps.com>
2018-07-05 02:26:22 -0700

    first commit

---
+++ demo1/service-helloworld/src/main/java/im/zero/HelloWorldApplication.java
@@ -0,0 +1,24 @@
+package im.zero;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name;
+    }
+
+}
